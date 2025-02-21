81fcab53632ce9882f5be839143bf1cf172ec4de
JanFehse <73786484+JanFehse@users.noreply.github.com>
2025-02-21 13:42:17 +0200

    Proto stuff fraud detection

--- utils/pb/fraud_detection/fraud_detection_pb2.pyi
+++ utils/pb/fraud_detection/fraud_detection_pb2.pyi
@@ -1,17 +1,33 @@
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
-from typing import ClassVar as _ClassVar, Optional as _Optional
+from typing import ClassVar as _ClassVar, Mapping as _Mapping, Optional as _Optional, Union as _Union
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
-class HelloRequest(_message.Message):
-    __slots__ = ("name",)
-    NAME_FIELD_NUMBER: _ClassVar[int]
-    name: str
-    def __init__(self, name: _Optional[str] = ...) -> None: ...
+class DetectFraudRequest(_message.Message):
+    __slots__ = ("CreditCardNumber", "BillingAdress")
+    CREDITCARDNUMBER_FIELD_NUMBER: _ClassVar[int]
+    BILLINGADRESS_FIELD_NUMBER: _ClassVar[int]
+    CreditCardNumber: str
+    BillingAdress: BillingAdress
+    def __init__(self, CreditCardNumber: _Optional[str] = ..., BillingAdress: _Optional[_Union[BillingAdress, _Mapping]] = ...) -> None: ...
 
-class HelloResponse(_message.Message):
-    __slots__ = ("greeting",)
-    GREETING_FIELD_NUMBER: _ClassVar[int]
-    greeting: str
-    def __init__(self, greeting: _Optional[str] = ...) -> None: ...
+class DetectFraudResponse(_message.Message):
+    __slots__ = ("isLegit",)
+    ISLEGIT_FIELD_NUMBER: _ClassVar[int]
+    isLegit: bool
+    def __init__(self, isLegit: bool = ...) -> None: ...
+
+class BillingAdress(_message.Message):
+    __slots__ = ("street", "city", "state", "zip", "country")
+    STREET_FIELD_NUMBER: _ClassVar[int]
+    CITY_FIELD_NUMBER: _ClassVar[int]
+    STATE_FIELD_NUMBER: _ClassVar[int]
+    ZIP_FIELD_NUMBER: _ClassVar[int]
+    COUNTRY_FIELD_NUMBER: _ClassVar[int]
+    street: str
+    city: str
+    state: str
+    zip: str
+    country: str
+    def __init__(self, street: _Optional[str] = ..., city: _Optional[str] = ..., state: _Optional[str] = ..., zip: _Optional[str] = ..., country: _Optional[str] = ...) -> None: ...
