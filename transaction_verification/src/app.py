7fb751b158d88d84fbc6550a5429e0dc2a905dab
Martin Beyerle <martin.beyerle@uni-konstanz.de>
2025-03-04 15:35:19 +0200

    usefull credit card check

--- transaction_verification/src/app.py
+++ transaction_verification/src/app.py
@@ -5,9 +5,7 @@ import os
 # The path of the stubs is relative to the current file, or absolute inside the container.
 # Change these lines only if strictly needed.
 FILE = __file__ if "__file__" in globals() else os.getenv("PYTHONFILE", "")
-grpc_path = os.path.abspath(
-    os.path.join(FILE, "../../../utils/pb")
-)
+grpc_path = os.path.abspath(os.path.join(FILE, "../../../utils/pb"))
 sys.path.insert(0, grpc_path)
 import shared.order_pb2 as order
 import shared.order_pb2_grpc as order_grpc
@@ -19,17 +17,37 @@ from concurrent import futures
 
 
 # Create a class to define the server functions, derived from
-# fraud_detection_pb2_grpc.HelloServiceServicer
+# transaction_verification_pb2_grpc.HelloServiceServicer
 class TransactionVerificationService(
     transaction_verification_grpc.TransactionVerificationServiceServicer
 ):
-    # Create an RPC function to say hello
+    # Create an RPC function to verify transaction
     def VerifyTransaction(self, request, context):
         # Create a HelloResponse object
         response = transaction_verification.TransactionResponse()
-        response.transactionVerified = True
+        if (
+            len(request.creditCard.number) == 16
+            and request.creditCard.number.isdigit()
+            and len(request.creditCard.cvv) == 3
+            and request.creditCard.cvv.isdigit()
+            and len(request.creditCard.expiryDate) == 5
+            and request.creditCard.expiryDate[2] == "/"
+            and request.creditCard.expiryDate[:2].isdigit()
+            and request.creditCard.expiryDate[3:].isdigit()
+            and request.creditCard.expiryDate[:2] <= "12"
+            and request.creditCard.expiryDate[1] > "0"
+            and request.creditCard.expiryDate[3:] > "25"
+        ):
+            response.transactionVerified = True
+            print(f"Verifizierte Transaktion mit Karte: {request.creditCard.number}")
+        else:
+            response.transactionVerified = False
+            print(
+                f"Transaktion mit Karte: {request.creditCard.number} konnte nicht verifiziert werden"
+            )
+        # response.transactionVerified = True
         # Print the greeting message
-        print(response.transactionVerified)
+        # print(response.transactionVerified)
         # Return the response object
         return response
 
