f455aacc22697a4cbf0e959697fa0df2810530c0
Mira Eulering <a51867661@unet.univie.ac.at>
2025-04-25 14:57:17 +0300

    paymentserver port

--- payment_service/src/app.py
+++ payment_service/src/app.py
@@ -65,7 +65,7 @@ def serve():
     server.add_insecure_port("[::]:" + port)
     # Start the server
     server.start()
-    print("Payment server started. Listening on port 50053.")
+    print("Payment server started. Listening on port 50056.")
     # Keep thread alive
     server.wait_for_termination()
 
