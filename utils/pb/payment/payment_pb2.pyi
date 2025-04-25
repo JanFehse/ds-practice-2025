51c7ba8b601ebfaed1d0d6abd511d9405cd9773e
JanFehse <73786484+JanFehse@users.noreply.github.com>
2025-04-25 14:46:22 +0300

    added id to payment

--- utils/pb/payment/payment_pb2.pyi
+++ utils/pb/payment/payment_pb2.pyi
@@ -7,13 +7,15 @@ from typing import ClassVar as _ClassVar, Mapping as _Mapping, Optional as _Opti
 DESCRIPTOR: _descriptor.FileDescriptor
 
 class PrepareRequest(_message.Message):
-    __slots__ = ("name", "CreditCard", "BillingAddress", "price")
+    __slots__ = ("name", "CreditCard", "BillingAddress", "price", "id")
     NAME_FIELD_NUMBER: _ClassVar[int]
     CREDITCARD_FIELD_NUMBER: _ClassVar[int]
     BILLINGADDRESS_FIELD_NUMBER: _ClassVar[int]
     PRICE_FIELD_NUMBER: _ClassVar[int]
+    ID_FIELD_NUMBER: _ClassVar[int]
     name: str
     CreditCard: _order_pb2.CreditCard
     BillingAddress: _order_pb2.BillingAddress
     price: int
-    def __init__(self, name: _Optional[str] = ..., CreditCard: _Optional[_Union[_order_pb2.CreditCard, _Mapping]] = ..., BillingAddress: _Optional[_Union[_order_pb2.BillingAddress, _Mapping]] = ..., price: _Optional[int] = ...) -> None: ...
+    id: int
+    def __init__(self, name: _Optional[str] = ..., CreditCard: _Optional[_Union[_order_pb2.CreditCard, _Mapping]] = ..., BillingAddress: _Optional[_Union[_order_pb2.BillingAddress, _Mapping]] = ..., price: _Optional[int] = ..., id: _Optional[int] = ...) -> None: ...
