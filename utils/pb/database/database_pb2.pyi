9d8b5a2b3cd22a22bf154ce7d27f73cea65388e5
JanFehse <73786484+JanFehse@users.noreply.github.com>
2025-04-25 14:27:10 +0300

    proto for databes/payment

---
+++ utils/pb/database/database_pb2.pyi
@@ -0,0 +1,42 @@
+from shared import order_pb2 as _order_pb2
+from google.protobuf import empty_pb2 as _empty_pb2
+from google.protobuf.internal import containers as _containers
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class ReadRequest(_message.Message):
+    __slots__ = ("title",)
+    TITLE_FIELD_NUMBER: _ClassVar[int]
+    title: str
+    def __init__(self, title: _Optional[str] = ...) -> None: ...
+
+class ReadResponse(_message.Message):
+    __slots__ = ("amount",)
+    AMOUNT_FIELD_NUMBER: _ClassVar[int]
+    amount: int
+    def __init__(self, amount: _Optional[int] = ...) -> None: ...
+
+class WriteRequest(_message.Message):
+    __slots__ = ("title", "new_stock")
+    TITLE_FIELD_NUMBER: _ClassVar[int]
+    NEW_STOCK_FIELD_NUMBER: _ClassVar[int]
+    title: str
+    new_stock: int
+    def __init__(self, title: _Optional[str] = ..., new_stock: _Optional[int] = ...) -> None: ...
+
+class ChangeAmountRequest(_message.Message):
+    __slots__ = ("title", "amount")
+    TITLE_FIELD_NUMBER: _ClassVar[int]
+    AMOUNT_FIELD_NUMBER: _ClassVar[int]
+    title: str
+    amount: int
+    def __init__(self, title: _Optional[str] = ..., amount: _Optional[int] = ...) -> None: ...
+
+class PrepareRequest(_message.Message):
+    __slots__ = ("books",)
+    BOOKS_FIELD_NUMBER: _ClassVar[int]
+    books: _containers.RepeatedCompositeFieldContainer[ChangeAmountRequest]
+    def __init__(self, books: _Optional[_Iterable[_Union[ChangeAmountRequest, _Mapping]]] = ...) -> None: ...
---
+++ utils/pb/payment/payment_pb2.pyi
@@ -0,0 +1,19 @@
+from shared import order_pb2 as _order_pb2
+from google.protobuf import empty_pb2 as _empty_pb2
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Mapping as _Mapping, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class PrepareRequest(_message.Message):
+    __slots__ = ("name", "CreditCard", "BillingAddress", "price")
+    NAME_FIELD_NUMBER: _ClassVar[int]
+    CREDITCARD_FIELD_NUMBER: _ClassVar[int]
+    BILLINGADDRESS_FIELD_NUMBER: _ClassVar[int]
+    PRICE_FIELD_NUMBER: _ClassVar[int]
+    name: str
+    CreditCard: _order_pb2.CreditCard
+    BillingAddress: _order_pb2.BillingAddress
+    price: int
+    def __init__(self, name: _Optional[str] = ..., CreditCard: _Optional[_Union[_order_pb2.CreditCard, _Mapping]] = ..., BillingAddress: _Optional[_Union[_order_pb2.BillingAddress, _Mapping]] = ..., price: _Optional[int] = ...) -> None: ...
